66cf61e1c76f8a6124f45030342c954b1c064d3b
Garm <melsom.henrik@gmail.com>
2023-04-07 02:44:32 +0200

    🚧 Work on pixel pencil

--- pixel_pencil.py
+++ pixel_pencil.py
@@ -1,25 +1,61 @@
 import bpy
-from bpy.types import WorkSpaceTool
+from bpy.types import WorkSpaceTool, Operator, GizmoGroup
+from typing import Union, Set
 
 
-class PixelPencil(WorkSpaceTool):
+class PixelPencil(Operator):
+    bl_idname = "pixel_perfect.pixel_pencil"
+    bl_label = "Pixel Pencil"
+
+    def modal(self, context, event) -> Union[Set[str], Set[int]]:
+        if event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE'}:
+            return {'PASS_THROUGH'}
+
+        return {'RUNNING_MODAL'}
+
+
+class PixelPencilWidgetGroup(GizmoGroup):
+    bl_idname = "VIEW3D_GGT_pixel_perfect_pixel_pencil"
+    bl_label = "Pixel Pencil Widget"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'WINDOW'
+    bl_options = {'3D', 'PERSISTENT'}
+
+    @classmethod
+    def poll(cls, context) -> bool:
+        wm = context.window_manager
+        op = wm.operators[-1] if wm.operators else None
+        if not isinstance(op, PixelPencil):
+            return False
+        return True
+    
+    def setup(self, context):
+        pass
+
+
+class PixelPencilTool(WorkSpaceTool):
     """Texture paint brush with pixel perfect accuracy."""
     bl_space_type = 'VIEW_3D'
     bl_context_mode = 'PAINT_TEXTURE'
-    bl_idname = "pixel_perfect.pixel_pencil"
+    bl_idname = "pixel_perfect.pixel_pencil_tool"
     bl_label = "Pixel Pencil"
     bl_description = "Texture paint brush with pixel perfect accuracy."
     bl_icon = "brush.paint_texture.draw"
     bl_widget = None
     bl_keymap = (
-        ("view3d.select_circle", {"type": 'LEFTMOUSE', "value": 'PRESS'},
+        ("pixel_perfect.pixel_pencil", {"type": 'LEFTMOUSE', "value": 'PRESS'},
          {"properties": [("wait_for_input", False)]}),
-        ("view3d.select_circle", {"type": 'LEFTMOUSE', "value": 'PRESS', "ctrl": True},
+        ("pixel_perfect.pixel_pencil", {"type": 'LEFTMOUSE', "value": 'PRESS', "ctrl": True},
          {"properties": [("mode", 'SUB'), ("wait_for_input", False)]}),
     )
 
+    def draw_settings(context, layout, tool):
+        props = tool.operator_settings("pixel_perfect.PixelPencil")
+
+
 def register():
-    bpy.utils.register_tool(PixelPencil, separator=True, group=True)
+    bpy.utils.register_tool(PixelPencilTool, separator=True, group=True)
+
 
 def unregister():
-    bpy.utils.unregister_tool(PixelPencil)
+    bpy.utils.unregister_tool(PixelPencilTool)
